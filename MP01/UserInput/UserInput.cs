630741c2f2ac9c72e7b9fde4eca91bfb6af0ce69
Vasil Hitrov <vhitrov@hotmail.com>
2015-10-13 12:28:03 +0300

    Модул 06, Видео 07 - Библиотека за първа формула

--- MP01/UserInput/UserInput.cs
+++ MP01/UserInput/UserInput.cs
@@ -7,6 +7,8 @@ namespace UserInput
 	{
 		About.Me about=new About.Me();
 		Colors.ForCLI _c=new Colors.ForCLI();
+		Calculations.MainLibrary _calc = new Calculations.MainLibrary();
+
 
 		public UInput ()
 		{
@@ -29,11 +31,8 @@ namespace UserInput
 				_c.Comand(); _command=Console.ReadLine();
 
 				//Проверка за налични команди
-				if(_command.ToLower().Contains("команда1")) 
-				{
-					_c.Defolt(); Console.Write("Стартирана е ");
-					_c.Result(); Console.WriteLine("команда 1\n");
-				}
+				if(_command.ToLower().Contains("яма")) _calc.F01.calc(_command);
+
 				if(_command.ToLower().Contains("команда2")) Console.WriteLine("Стартирана е команда 2\n");
 				if(_command.ToLower().Contains("команда3")) Console.WriteLine("Стартирана е команда 3\n");
 
